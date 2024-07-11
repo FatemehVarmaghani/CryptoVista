76603175015940fa66d2f214bd79f50912c5c190
Fatemeh <fatemehvarmaghani.main@gmail.com>
2024-07-11 22:46:36 +0330

    home and explore toolbar icons set (except for search's action)

---
+++ app/src/main/java/com/example/cryptovista/Constatnts.kt
@@ -0,0 +1,6 @@
+package com.example.cryptovista
+
+const val HOME_TITLE = "Top Coins"
+const val NEWS_TITLE = "News"
+const val EXPLORE_TITLE = "Explore"
+const val PROFILE_TITLE = "Profile"
--- app/src/main/java/com/example/cryptovista/features/activities/MainActivity.kt
+++ app/src/main/java/com/example/cryptovista/features/activities/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
+import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.cryptovista.EXPLORE_TITLE
+import com.example.cryptovista.HOME_TITLE
+import com.example.cryptovista.NEWS_TITLE
+import com.example.cryptovista.PROFILE_TITLE
 import com.example.cryptovista.R
 import com.example.cryptovista.databinding.ActivityMainBinding
 import com.example.cryptovista.features.fragments.ExploreFragment
 import com.example.cryptovista.features.fragments.HomeFragment
 import com.example.cryptovista.features.fragments.NewsFragment
 import com.example.cryptovista.features.fragments.ProfileFragment
-import com.example.cryptovista.network.ApiManager
-import com.example.cryptovista.network.model.CoinList
 
 const val FROM_MAIN_ACTIVITY = "from_main_activity"
 
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         firstRun()
         handleBottomNavigation()
+        setSupportActionBar(binding.mainToolbar)
 
         binding.mainToolbar.setNavigationOnClickListener {
             //show drawer navigation
@@ -37,9 +40,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.mainToolbar.setOnMenuItemClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coingecko.com/"))
-            startActivity(intent)
-            true
+            when (it.itemId) {
+                R.id.item_more_home, R.id.item_more_explore -> {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coingecko.com/"))
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.item_search_explore -> {
+                    showToast("search:)")
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
         }
     }
 
@@ -57,21 +73,25 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
+                    binding.mainToolbar.title = HOME_TITLE
                     fragmentTransaction(R.id.main_container, HomeFragment())
                     true
                 }
 
                 R.id.nav_news -> {
+                    binding.mainToolbar.title = NEWS_TITLE
                     fragmentTransaction(R.id.main_container, NewsFragment())
                     true
                 }
 
                 R.id.nav_explore -> {
+                    binding.mainToolbar.title = EXPLORE_TITLE
                     fragmentTransaction(R.id.main_container, ExploreFragment())
                     true
                 }
 
                 R.id.nav_profile -> {
+                    binding.mainToolbar.title = PROFILE_TITLE
                     fragmentTransaction(R.id.main_container, ProfileFragment())
                     true
                 }
@@ -81,6 +101,33 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        when {
+            binding.bottomNavigation.menu.findItem(R.id.nav_home).isChecked -> {
+                binding.mainToolbar.menu.clear()
+                menuInflater.inflate(R.menu.menu_toolbar_home, menu)
+            }
+
+            binding.bottomNavigation.menu.findItem(R.id.nav_news).isChecked -> {
+                binding.mainToolbar.menu.clear()
+            }
+
+            binding.bottomNavigation.menu.findItem(R.id.nav_explore).isChecked -> {
+                binding.mainToolbar.menu.clear()
+                menuInflater.inflate(R.menu.menu_toolbar_explore, menu)
+            }
+
+            binding.bottomNavigation.menu.findItem(R.id.nav_profile).isChecked -> {
+                binding.mainToolbar.menu.clear()
+            }
+
+            else -> {}
+        }
+
+        return true
     }
 
     private fun showToast(message: String) {
